96259019fec34a13c45aa8bcdc497c3e9ef273dd
nikhilghosh75 <nighosh@umich.edu>
2020-05-01 21:27:49 -0400

    Added Despawn and SpawnParticleOnEnable

---
+++ Assets/Scripts/VFX/Despawn.cs
@@ -0,0 +1,52 @@
+﻿/** 
+ * Despawns a gameobject after a specified amount of time.
+ * @ Alex Kisil '19
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSoft.VFX
+{
+    public class Despawn : MonoBehaviour
+    {
+        [Tooltip("Automatically Despawn after a certain amount of time")]
+        public bool autoDespawn = false;
+
+        [Tooltip("The time between start and destroy, in seconds, if autoDespawn is true")]
+        public float timeToAutoDespawn = 5f;
+
+        [Tooltip("The time between the Early Despawn call and the actual destruction of the object, in seconds")]
+        public float earlyDespawnTime = 3f;
+
+        void Start()
+        {
+            if (autoDespawn)
+            {
+                StartCoroutine(Predespawn(timeToAutoDespawn));
+            }
+        }
+
+        /// <summary>
+        /// Destroy the object before the automatic despawn time
+        /// Note that this function does not halt the automatic destruction of the object, if autoDespawn is set to true.
+        /// </summary>
+        public void EarlyDespawn()
+        {
+            StartCoroutine(Predespawn(earlyDespawnTime));
+        }
+
+        IEnumerator Predespawn(float despawnTime)
+        {
+            while (true)
+            {
+                // Suspend execution for despawnTime seconds
+                yield return new WaitForSeconds(despawnTime);
+                StopAllCoroutines();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/VFX/SpawnParticleOnEnable.cs
@@ -0,0 +1,140 @@
+﻿/*
+ * This script controls particle system by spawning particles upon enabling
+ * Attach this script directly onto the projectile being spawned
+ * Originally designed for the boss system, but has been more generalized
+ * @ David Sohn '20
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSoft.VFX
+{
+    public class SpawnParticleOnEnable : MonoBehaviour
+    {
+        [Tooltip("The delay when killing the primary particle systems")]
+        [SerializeField] private float killDelay;
+
+        [Tooltip("Should the particles be parented to the object spawning it")]
+        [SerializeField] private bool parentToObject;
+
+        [Tooltip("A list of particles to be initially spawned")]
+        [SerializeField] private List<GameObject> initialParticles;
+
+        [Tooltip("A list of references to the primary particles spawned in the game world")]
+        private List<GameObject> initialParticlesReferences;
+
+        [Tooltip("The offset of the primary particles to be spawned from the game object")]
+        [SerializeField] private Vector3 initialParticleOffset;
+
+        [Space]
+
+        [Tooltip("Should the script spawn secondary particles")]
+        [SerializeField] private bool spawnSecondaryParticles;
+
+        [Tooltip("The delay when killing the secondary particle systems, if secondary particles are to be spawned.")]
+        [SerializeField] private float secondaryKillDelay;
+
+        [Tooltip("Should the secondary particle system be parented to the object spawning it")]
+        [SerializeField] private bool parentToObjectSecondary;
+
+        [Tooltip("A list of secondary particles to be initially spawned")]
+        [SerializeField] private List<GameObject> secondaryParticles;
+
+        [Tooltip("A list of references to the secondary particles spawned in the game world")]
+        private List<GameObject> secondaryParticlesReferences;
+
+        [Tooltip("The offset of the secondary particles to be spawned from the game object")]
+        [SerializeField] private Vector3 secondaryParticleOffset;
+
+        void Awake()
+        {
+            initialParticlesReferences = new List<GameObject>();
+            secondaryParticlesReferences = new List<GameObject>();
+        }
+
+        public void OnEnable()
+        {
+            foreach (GameObject particle in initialParticles)
+            {
+                GameObject newParticle;
+                if (parentToObject)
+                {
+                    newParticle = Instantiate(particle, transform.position + initialParticleOffset, Quaternion.identity);
+                    newParticle.transform.parent = transform;
+                }
+                else
+                {
+                    newParticle = Instantiate(particle, transform.position + initialParticleOffset, Quaternion.identity);
+                }
+                initialParticlesReferences.Add(newParticle);
+                newParticle.transform.parent = this.gameObject.transform;
+            }
+        }
+
+
+        /// <summary>
+        /// Spawns the secondary particles.  Activate with the event in the boss projectile.
+        /// </summary>
+        public void SpawnParticles()
+        {
+            foreach (GameObject particle in secondaryParticles)
+            {
+                GameObject newParticle;
+                if (parentToObjectSecondary)
+                {
+                    newParticle = Instantiate(particle, transform.position + secondaryParticleOffset,
+                     Quaternion.identity);
+                    newParticle.transform.parent = transform;
+                }
+                else
+                    newParticle = Instantiate(particle, transform.position + secondaryParticleOffset,
+                     Quaternion.identity);
+                secondaryParticlesReferences.Add(newParticle);
+                newParticle.transform.parent = this.gameObject.transform;
+            }
+        }
+
+        /// <summary>
+        /// Removes particles.  Activate with the event in the boss projectile.
+        /// </summary>
+        public void KillPrimaryParticles()
+        {
+            StartCoroutine(KillParticlesAfterDelay(true));
+        }
+
+        /// <summary>
+        /// Kill all the secondary particles
+        /// </summary>
+        public void KillSecondaryParticles()
+        {
+            StartCoroutine(KillParticlesAfterDelay(false));
+        }
+
+        IEnumerator KillParticlesAfterDelay(bool killPrimary)
+        {
+            if (killPrimary)
+            {
+                yield return new WaitForSeconds(killDelay);
+                foreach (GameObject particle in initialParticlesReferences)
+                {
+                    if (particle != null)
+                        Destroy(particle);
+                }
+            }
+            else
+            {
+                yield return new WaitForSeconds(secondaryKillDelay);
+                if (spawnSecondaryParticles)
+                {
+                    foreach (GameObject particle in secondaryParticlesReferences)
+                    {
+                        if (particle != null)
+                            Destroy(particle);
+                    }
+                }
+            }
+        }
+    }
+}
