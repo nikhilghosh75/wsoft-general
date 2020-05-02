236487eb6f0bce4e004d7e5a6da171027a78e2bf
nikhilghosh75 <nighosh@umich.edu>
2020-05-01 21:26:59 -0400

    Added Layermask Functions

---
+++ Assets/Scripts/Math/LayermaskFunctions.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * A set of static functions used for interacting with layermasks without requiring the knowledge of layers.
+ * @ Nikhil Ghosh '23
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSoft.Math
+{
+    public class LayermaskFunctions
+    {
+        /// <summary>
+        /// Checks if a layer is in a given layermask
+        /// </summary>
+        /// <param name="mask">The mask to check</param>
+        /// <param name="layer">The layer to check</param>
+        /// <returns>Is the layer in the layermask</returns>
+        public static bool IsInLayerMask(LayerMask mask, int layer)
+        {
+            if(layer < 0 || layer > 32)
+            {
+                return false;
+            }
+            return (((mask.value) >> layer) % 2) == 1;
+        }
+    }
+}
