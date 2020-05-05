c7210e79729e4984b4385678b6910f3f6119b8e9
nikhilghosh75 <nighosh@umich.edu>
2020-05-04 21:26:04 -0400

    Added Brightness and SliderText scripts

---
+++ Assets/Scripts/UI/Brightness.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WSoft.UI
+{
+    public class Brightness : MonoBehaviour
+    {
+        [SerializeField] Slider brightnessSlider;
+        Image brightnessOverlay;
+
+
+        void Awake()
+        {
+            brightnessOverlay = GetComponent<UnityEngine.UI.Image>();
+
+            if (PlayerPrefs.HasKey("brightness"))
+            {
+                SetBrightness(PlayerPrefs.GetFloat("brightness"));
+            }
+            else
+            {
+                SetBrightness(1);
+            }
+        }
+
+        /// <summary>
+        /// Sets the brightness from a given slider value
+        /// </summary>
+        /// <param name="slider">The slider to take the normalized value of</param>
+        public void SetBrightnessFromSlider(Slider slider)
+        {
+            SetBrightness(slider.normalizedValue);
+        }
+
+        /// <summary>
+        /// Set the screen brightness
+        /// </summary>
+        /// <param name="value">A float between 0 and 1</param>
+        public void SetBrightness(float value)
+        {
+            value = Mathf.Clamp01(value);
+            Debug.Log("brightness set to " + value);
+            PlayerPrefs.SetFloat("brightness", value);
+            brightnessSlider.SetValueWithoutNotify(value * brightnessSlider.maxValue);
+
+            float brightness = .5f + value / 2;
+            Color c = brightnessOverlay.color;
+            c.a = 1 - brightness;
+            brightnessOverlay.color = c;
+        }
+    }
+}
+
---
+++ Assets/Scripts/UI/SliderText.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Augments the behavior of UnityEngine.UI.Slider by changing the text of a TextComponent to match the value.
+ * @ Jacob Shreve '?, Nikhil Ghosh '23
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSoft.UI
+{
+    public class SliderText : MonoBehaviour
+    {
+        [Tooltip("The text to be modified")]
+        public UnityEngine.UI.Text valueText;
+
+        void Start()
+        {
+            UnityEngine.UI.Slider slider = GetComponent<UnityEngine.UI.Slider>();
+            slider.onValueChanged.AddListener(OnValueChanged);
+            valueText.text = slider.value.ToString();
+        }
+
+        void OnValueChanged(float value)
+        {
+            valueText.text = value.ToString();
+        }
+    }
+}
