00d6ec8c5d4b4fd3be435e08f72504b1868f8f20
nikhilghosh75 <nighosh@umich.edu>
2020-05-04 21:16:33 -0400

    Added SliderAudio and VolumeController scripts

---
+++ Assets/Scripts/Audio/SliderAudio.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Auguments the behavior of UnityEngine.UI.Slider by adding AudioEvents to when the value changes.
+ * @ Jacob Shreve '?, William Bostick '21
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSoft.Audio
+{
+    public class SliderAudio : MonoBehaviour
+    {
+        private float previousValue;
+        [SerializeField] AudioEvent sliderIncrease;
+        [SerializeField] AudioEvent sliderDecrease;
+
+        void Start()
+        {
+            UnityEngine.UI.Slider slider = GetComponent<UnityEngine.UI.Slider>();
+            slider.onValueChanged.AddListener(OnValueChanged);
+            float value = slider.value;
+            previousValue = value;
+        }
+
+        /// <summary>
+        /// On Value Changed, play the corresponding audio.
+        /// </summary>
+        /// <param name="value">The current value of the slider</param>
+        void OnValueChanged(float value)
+        {
+            // fire off correct AudioEvent
+            if (previousValue > value)
+            {
+                sliderDecrease.PostWwiseEvent(gameObject);
+            }
+            else
+            {
+                sliderIncrease.PostWwiseEvent(gameObject);
+            }
+            previousValue = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/VolumeController.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Allows you to control the volume through UnityEngine.UI.Slider objects and WWise RTPC
+ * @ Jacob Shreve '?, @ Nikhil Ghosh '23
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSoft.Audio
+{
+    public struct VolumeData
+    {
+        [Tooltip("The slider that controls the volume")]
+        public UnityEngine.UI.Slider volumeSlider;
+
+        [Tooltip("The name of the RTPC in Wwise. Note that this will be the same name for PlayerPrefs")]
+        public string volumeRTPC;
+
+        /// <summary>
+        /// Set the RTPC and PlayerPrefs value for the volume when the value on a slider has changed
+        /// </summary>
+        /// <param name="newValue">The new value for the RTPC</param>
+        public void SetVolumeFromValue(float newValue)
+        {
+            float realVolume = Mathf.Clamp01(newValue);
+            Debug.Log(volumeRTPC + " set to " + realVolume);
+            volumeSlider.SetValueWithoutNotify(realVolume * volumeSlider.maxValue);
+            PlayerPrefs.SetFloat(volumeRTPC, realVolume);
+            AkSoundEngine.SetRTPCValue(volumeRTPC, realVolume);
+        }
+    }
+
+    public class VolumeController : MonoBehaviour
+    {
+        [Tooltip("A list of volumes that can be controlled")]
+        public List<VolumeData> volumesToControl;
+
+        void Start()
+        {
+            foreach(VolumeData volume in volumesToControl)
+            {
+                if (PlayerPrefs.HasKey(volume.volumeRTPC))
+                {
+                    volume.SetVolumeFromValue(PlayerPrefs.GetFloat(volume.volumeRTPC));
+                }
+                else
+                {
+                    volume.SetVolumeFromValue(0.5f);
+                }
+                volume.volumeSlider.onValueChanged.AddListener(volume.SetVolumeFromValue);
+            }
+        }
+    }
+}
