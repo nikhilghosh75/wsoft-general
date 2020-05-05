5d53c5ebf14f4bdc90fbd84ce4c398ea0a9ed0b6
nikhilghosh75 <nighosh@umich.edu>
2020-05-04 21:20:21 -0400

    Added EnemyAggro and OnHurtSound scripts

---
+++ Assets/Scripts/Combat/EnemyAggro.cs
@@ -0,0 +1,133 @@
+﻿/* Handles when enemies enter aggro and the resulting actions taken.
+ * This is used for activating the enemies.
+ * @ Michael Tilbury (zafuru#8634)
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace WSoft.Combat
+{
+    public class EnemyAggro : MonoBehaviour
+    {
+        [Header("Enemy Aggro Trigger")]
+
+        [Tooltip("The enemy that the Aggro Range belongs to.")]
+        [SerializeField] private GameObject enemyToFollow;
+
+        [Tooltip("The radius of the circle collider")]
+        private float radius;
+
+        [Tooltip("Should the enemy follow up with a raycast when the player enters the trigger")]
+        [SerializeField] bool raycastLineOfSight = false;
+
+        [Tooltip("The layermask to raycast against")]
+        [SerializeField] LayerMask raycastLayerMask;
+
+        [Tooltip("Is the player in the collider but not found. Only applies if RaycastLineOfSIght is true")]
+        private bool isSearching = false;
+
+        private GameObject target;
+
+        [Space(10)]
+
+        [Header("Enemy Aggro Events")]
+        // Invoked when player enters aggro range on this enemy.
+        public UnityEvent OnAggro;
+
+        // Invoked when player exits aggro range on this enemy.
+        public UnityEvent OnDeaggro;
+
+        private void Start()
+        {
+            // Get reference to enemy's CircleCollider2D
+            CircleCollider2D collider = GetComponent<CircleCollider2D>();
+
+            // Set the enemy's circle collider to specified aggroRadius
+            collider.isTrigger = true;
+            radius = collider.radius;
+        }
+
+        private void Update()
+        {
+            transform.position = enemyToFollow.transform.position;
+        }
+
+        /// <summary>
+        /// Gets the target of the Aggro
+        /// </summary>
+        /// <returns>The target of the aggro (Usually the player)</returns>
+        public GameObject GetTarget()
+        {
+            return target;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            // Invoke OnAggro when the player enters the enemy's trigger or enter search mode
+            if (other.gameObject.CompareTag("Player"))
+            {
+
+                if (raycastLineOfSight)
+                {
+                    isSearching = true;
+                }
+                else
+                {
+                    target = other.gameObject;
+                    OnAggro.Invoke();
+                }
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            // Raycast until enemy has line of sight on the player
+            if (raycastLineOfSight && isSearching)
+            {
+                if (other.gameObject.CompareTag("Player") && HasLineOfSight(other.gameObject))
+                {
+                    OnAggro.Invoke();
+                    isSearching = false;
+                }
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            // Invoke OnDeaggro when the player exits the enemy's trigger 
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (!raycastLineOfSight)
+                {
+                    OnDeaggro.Invoke();
+                }
+                else if (raycastLineOfSight && !isSearching)
+                {
+                    OnDeaggro.Invoke();
+                }
+            }
+        }
+
+        // This raycasts to the target position up to the aggro radius and uses the passed in layermask
+        private bool HasLineOfSight(GameObject target)
+        {
+            // Get direction and raycast to aggro radius
+            Vector2 direction = (target.transform.position - this.transform.position).normalized;
+            RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction, radius, raycastLayerMask);
+
+            // Check for raycast and that hit is player
+            if (hit.collider != null && hit.collider.gameObject.CompareTag("Player"))
+            {
+                target = hit.collider.gameObject;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Combat/OnHurtSound.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Play Sound when the enemy is hurt
+ * @ Ziang Wang
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WSoft.Combat
+{
+    public class OnHurtSound : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        Health health;
+
+        [Tooltip("The sound to play")]
+        public WSoft.Audio.AudioEvent audioEvent;
+
+        void Start()
+        {
+            health.GetComponent<Health>();
+            health.events.OnDamage.AddListener(PlaySound);
+        }
+
+        /// <summary>
+        /// Play the specified AudioEvent
+        /// </summary>
+        void PlaySound()
+        {
+            audioEvent.PostWwiseEvent(this.gameObject);
+        }
+    }
+}
